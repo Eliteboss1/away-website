30b2e6f610b8d9914f2fa5b833550c5eb04051c3
Daniel Ipogah <darnielipogah@gmail.com>
2022-12-31 03:08:09 +0100

    install depencies for project

---
+++ away-website/src/App.js
@@ -0,0 +1,10 @@
+import Away from "./Away";
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <Away />
+    </div>
+  );
+}
---
+++ away-website/src/Away.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Away() {
+  return (
+    <div>
+      <div className="bg-[url('\src\assets\Rectangle 3.jpg')]"> 33hello</div>
+    </div>
+  );
+}
