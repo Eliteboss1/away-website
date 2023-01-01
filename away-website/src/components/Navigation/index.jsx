adc94726aaa15b81cc189ba7ce26a827fc2325c7
Ebiperre Iyoro <iyorop@yahoo.com>
2023-01-01 23:12:30 +0100

    Update index.jsx

--- away-website/src/components/Navigation/index.jsx
+++ away-website/src/components/Navigation/index.jsx
@@ -4,7 +4,7 @@ import { AppLogo } from '../../utils/assets'
 const index = () => {
   return (
     <div className="bg-hero">
-        <div className="grid grid-cols-2 pt-10 px-10">
+        <div className="grid grid-cols-2 pt-10 px-10 items-center">
             {/* left */}
             <div className="">
                 <img src={AppLogo} alt="" />
