91d09e81c871489553fcc59e9170984c5b6ad518
Ebiperre Iyoro <iyorop@yahoo.com>
2023-01-02 00:29:48 +0100

    hero section /1

--- away-website/src/pages/Home/Hero/index.jsx
+++ away-website/src/pages/Home/Hero/index.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Right } from "../../../utils/assets";
 
 const index = () => {
   return (
-    <div>
-        {/* wrapper */}
-        <div>
-            <span>Experience</span>
-            <span>Africa</span>
-        </div>
+    <div className="pt-60 px-16">
+      {/* wrapper */}
+      <div className="flex flex-col py-14">
+        <span className="text-9xl text-white font-bold">Experience</span>
+        <span className="text-9xl text-white font-bold">Africa</span>
+        <button className="rounded-xl bg-orange text-white p-3 w-80 h-20 text-3xl mt-5 flex justify-center items-center">
+          BOOK A TRIP <img src={Right} alt="" className="pl-4"/>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
--- away-website/src/utils/assets.js
+++ away-website/src/utils/assets.js
@@ -1,7 +1,9 @@
 import AppLogo from "../assets/away.svg"
 import HeroBg from "../assets/Rectangle 3.svg"
+import Right from "../assets/right.svg"
 
 export {
     AppLogo,
-    HeroBg
+    HeroBg,
+    Right
 }
