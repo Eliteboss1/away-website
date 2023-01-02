244dae6b700fb30b440dacd073043b172526aebc
Ebiperre Iyoro <iyorop@yahoo.com>
2023-01-03 00:32:43 +0100

    footer pt1

--- away-website/src/components/Footer/index.jsx
+++ away-website/src/components/Footer/index.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from "react";
+import { AppLogoWhite, Facebook, Instagram, Twitter } from "../../utils/assets";
 
 const index = () => {
   return (
-    <div>index</div>
-  )
-}
+    <div className="bg-black">
+      {/* wrapper */}
+      <div className="flex px-10">
+        {/* left side */}
+        <div className="flex-1">
+          <img src={AppLogoWhite} alt="" />
+        </div>
 
-export default index
+        {/* right side */}
+        <div className="flex-[4]">
+          <div className="grid grid-cols-4">
+            {/* General */}
+            <div className="text-white">
+              <p className="font-medium text-lg mb-1">General</p>
+              <ul>
+                <li>Discover</li>
+                <li>Morocco</li>
+                <li>Sahara</li>
+                <li>Ethiopia</li>
+                <li>North Africa</li>
+                <li>Victoria Fails</li>
+              </ul>
+            </div>
+
+            {/* Company */}
+            <div className="text-white">
+                <p className="font-medium text-lg mb-1">Company</p>
+                <ul>
+                    <li>About Us</li>
+                    <li>Safety</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+
+            {/* Community */}
+            <div className="text-white">
+                <p className="text-medium text-lg mb-1">Community</p>
+                <ul>
+                    <li>Staying Safe On & Offline </li>
+                    <li>Frequent Questions</li>
+                    <li>Travel Advice</li>
+                </ul>
+            </div>
+
+            {/* Legal */}
+            <div className="text-white">
+                <p className="text-medium text-lg mb-1">Legal</p>
+                <ul>
+                    <li>Terms & Conditions</li>
+                    <li>Booking Terms</li>
+                    <li>Privacy Policy</li>
+                    <li>FFI Policy</li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      {/* bottom items */}
+
+      <div className="flex items-center justify-center">
+        <p className="text-white px-10">© 2022 Away Limited. All Rights Reserved.</p>
+
+        {/* Social links */}
+        <div className="flex w-4 h-4 gap-2 float-right">
+            <img src={Facebook} alt="" />
+            <img src={Instagram} alt="" />
+            <img src={Twitter} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
--- away-website/src/utils/assets.js
+++ away-website/src/utils/assets.js
@@ -1,21 +1,21 @@
-<<<<<<< HEAD
-import AppLogo from "../assets/away.svg";
-import HeroBg from "../assets/Rectangle 3.svg";
-import Right from "../assets/right.svg";
-
-export { AppLogo, HeroBg, Right };
-=======
 import AppLogo from "../assets/away.svg"
+import AppLogoWhite from "../assets/away (1).svg"
 import HeroBg from "../assets/Rectangle 3.svg"
 import Right from "../assets/right.svg"
 import Arrow from "../assets/Group 24.svg"
 import GroupedImages from "../assets/Group 25.svg"
+import Facebook from "../assets/facebook.svg"
+import Instagram from "../assets/Instagram (icon — Colour).svg"
+import Twitter from "../assets/Twitter (icon — Colour).svg"
 
 export {
     AppLogo,
+    AppLogoWhite,
     HeroBg,
     Right,
     Arrow,
-    GroupedImages
+    GroupedImages,
+    Facebook,
+    Instagram,
+    Twitter
 }
->>>>>>> b020b8b56592d18c05086d5a2d33ce301641202f
