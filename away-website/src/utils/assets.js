ac053f29889b4abf604c5b45f912c8e96d4799ef
Daniel Ipogah <darnielipogah@gmail.com>
2023-01-03 13:55:15 +0100

    change section feature

--- away-website/src/utils/assets.js
+++ away-website/src/utils/assets.js
@@ -1,21 +1,7 @@
-<<<<<<< HEAD
 import AppLogo from "../assets/away.svg";
 import HeroBg from "../assets/Rectangle 3.svg";
 import Right from "../assets/right.svg";
+import Arrow from "../assets/Group 24.svg";
+import GroupedImages from "../assets/Group 25.svg";
 
-export { AppLogo, HeroBg, Right };
-=======
-import AppLogo from "../assets/away.svg"
-import HeroBg from "../assets/Rectangle 3.svg"
-import Right from "../assets/right.svg"
-import Arrow from "../assets/Group 24.svg"
-import GroupedImages from "../assets/Group 25.svg"
-
-export {
-    AppLogo,
-    HeroBg,
-    Right,
-    Arrow,
-    GroupedImages
-}
->>>>>>> b020b8b56592d18c05086d5a2d33ce301641202f
+export { AppLogo, HeroBg, Right, Arrow, GroupedImages };
